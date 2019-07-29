224bdbc93f156a9a08c2e94be0c295123f07a993
Sam Clements <sam.clements@infosum.com>
2019-07-29 15:21:50 +0100

    Add tests for nested data access

--- legere/record.py
+++ legere/record.py
@@ -1,32 +1,29 @@
 import dataclasses
 import json
-import string
 import textwrap
 import typing
-import collections
 
 import click
 
 from legere.colors import color
 
-JSONValue = typing.Union[
-    str, int, float, bool, typing.Mapping[str, typing.Any], typing.Sequence
-]
-JSON = typing.Mapping[str, str]
+RecordJSONValue = typing.Union[str, int, float, bool, typing.Sequence, typing.Mapping]
 
 
-class AttrDict(collections.UserDict):
+class RecordDict(dict, typing.Mapping[str, RecordJSONValue]):
+    """A mapping that allows access to values as if they were attributes."""
+
     def __getattr__(self, item) -> typing.Any:
         return self[item]
 
 
 @dataclasses.dataclass()
 class Record:
-    data: JSON
+    data: RecordDict
 
     @classmethod
     def from_string(cls, value: str):
-        return cls(json.loads(value, object_hook=AttrDict))
+        return cls(json.loads(value, object_hook=RecordDict))
 
     def format(
         self,
@@ -39,7 +36,7 @@ class Record:
         blocks: typing.Tuple[str, ...] = tuple(self.blocks(multiline_keys))
 
         # Extract a level and use it to color the record if possible.
-        level_value = self.data.get(level_key) if level_key else None
+        level_value = self.extract(level_key) if level_key else None
         record = color(level_value, record) if level_value else record
 
         if blocks:
@@ -48,19 +45,39 @@ class Record:
 
         return record
 
+    def extract(self, key: str) -> RecordJSONValue:
+        result = self.data
+        for k in key.split("."):
+            result = result[k]
+        return result
+
     def blocks(self, multiline_keys: typing.Sequence[str]) -> typing.Iterator[str]:
         indent = " " * 4
         width, _ = click.get_terminal_size()
         width = width - 2 * len(indent)
 
         for key in multiline_keys:
-            value_format = string.Template("{${key}}").substitute(key=key)
-            value: str = value_format.format_map(self.data)
+            value = self.extract(key)
+
             if value:
-                lines = self.wrap_lines(value, width)
+                string = self.value_to_string(value)
+                lines = self.wrap_lines(string, width)
                 lines = (click.style(l, dim=True) for l in lines)
                 lines = (indent + l for l in lines)
                 yield "\n".join(lines)
+
+    @staticmethod
+    def value_to_string(value: RecordJSONValue) -> str:
+        """
+        Transform a JSON value into something we can display in a multiline block.
+
+        Strings are left alone (as we don't want to add quotes) and any other values are
+        dumped as indented JSON.
+        """
+        if isinstance(value, str):
+            return value
+
+        return json.dumps(value, indent=2)
 
     @staticmethod
     def wrap_lines(lines: str, width: int) -> typing.Iterable[str]:
--- legere/tests/test_record.py
+++ legere/tests/test_record.py
@@ -3,43 +3,62 @@ import typing
 
 import pytest
 
-from legere.record import Record
+from legere.record import Record, RecordDict
 
 
 @dataclasses.dataclass()
 class Example:
-    record: Record
+    line: str
     expected: str
     format_string: str
     level_key: typing.Optional[str] = None
     multiline_keys: typing.Sequence[str] = ()
+
+    def record(self):
+        return Record.from_string(self.line)
 
 
 @pytest.mark.parametrize(
     "example",
     [
         Example(
-            Record({"timestamp": "2019-06-26", "message": "Hello World"}),
+            line='{"timestamp": "2019-06-26", "message": "Hello World"}',
             expected="2019-06-26 Hello World",
             format_string="{timestamp} {message}",
         ),
         Example(
-            Record({"@timestamp": "2019-06-26", "@message": "Hello World"}),
+            line='{"@timestamp": "2019-06-26", "@message": "Hello World"}',
             expected="2019-06-26 Hello World",
             format_string="{@timestamp} {@message}",
         ),
+        # Test message coloring.
         Example(
-            Record({"level": "CRITICAL", "message": "Hello World"}),
+            line='{"message": "Hello World", "level": "CRITICAL"}',
             expected="\x1b[31m\x1b[1mCRITICAL Hello World\x1b[0m",
             format_string="{level} {message}",
             level_key="level",
         ),
+        # Test nested keys can be used in both the format string and config keys.
+        Example(
+            line='{"nested": {"message": "Hello World", "multiline": "Lorem Ipsum", "level": "CRITICAL"}}',
+            expected="\x1b[31m\x1b[1mHello World\x1b[0m\n\n    \x1b[2mLorem Ipsum\x1b[0m\n",
+            format_string="{nested.message}",
+            level_key="nested.level",
+            multiline_keys=["nested.multiline"],
+        ),
+        # Test nested data renders nicely.
+        Example(
+            line='{"nested": {"a": 1, "b": "Z", "c": []}}',
+            expected='static\n\n    \x1b[2m{\x1b[0m\n    \x1b[2m  "a": 1,\x1b[0m\n    \x1b[2m  "b": "Z",\x1b[0m\n    \x1b[2m  "c": []\x1b[0m\n    \x1b[2m}\x1b[0m\n',
+            format_string="static",
+            multiline_keys=["nested"],
+        ),
     ],
 )
 def test_examples(example: Example):
-    actual = example.record.format(
+    actual = example.record().format(
         format_string=example.format_string,
         level_key=example.level_key,
         multiline_keys=example.multiline_keys,
     )
-    assert actual == example.expected
+    assert actual == example.expected, repr(actual)
