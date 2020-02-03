c195d8196a5b31029d19d8803cf2746657469f79
Sam Clements <sam@borntyping.co.uk>
2020-02-03 10:08:08 +0000

    Better parsing of ElasticSearch's buffered JSON

--- jsonlog_cli/multiline.py
+++ jsonlog_cli/multiline.py
@@ -14,20 +14,39 @@ class BufferedJSONStream:
     def __iter__(self):
         self.reset_buffer()
         for line in self.stream:
-            self.buffer += line
-            if self.buffer_is_complete():
-                yield self.buffer
-                self.reset_buffer()
+            # Yield any remaining lines in the buffer if the current
+            # line parses as JSON or starts with a '{' character.
+            if self.is_valid_json(line):
+                yield from self.reset_buffer()
 
+                # This is a small optimisation to avoid checking if the line
+                # contains JSON a second time when we add it to the empty buffer.
+                yield line
+                continue
+
+            # This stops us from buffering forever if we start in the middle
+            # of a JSON message, since we'd just keep adding new lines.
+            if line.startswith("{"):
+                yield from self.reset_buffer()
+
+            # Add the line to the buffer, then yield lines if the buffer
+            # now contains JSON. This should yield in most cases.
+            self.buffer += line
+            if self.is_valid_json(self.buffer):
+                yield from self.reset_buffer()
+
+        # Yield any remaining lines in the buffer.
+        yield from self.reset_buffer()
+
+    def reset_buffer(self) -> typing.Iterator[str]:
         if self.buffer:
             yield self.buffer
-
-    def reset_buffer(self) -> None:
         self.buffer = ""
 
-    def buffer_is_complete(self) -> bool:
+    @staticmethod
+    def is_valid_json(text: str) -> bool:
         try:
-            json.loads(self.buffer)
+            json.loads(text)
         except json.JSONDecodeError:
             return False
         else:
